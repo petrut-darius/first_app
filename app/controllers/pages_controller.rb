0c00f824e0a50c8bf2122f3451f752caf3ff11f4
Petrut Darius <eminoviciidarius@gmail.com>
2025-05-08 11:21:45 +0300

    Added links and a services page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def hello
+  end
+
+  def about
+  end
+
+  def services
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  root "pages#home", as: "home"# scrie de la sine _controller
+  #, as: "x" -> x o referinta
+  get "about", to: "pages#about", as: "about"# pune de la sine .erb sau ce trebuie sa puna
+  get "services", to: "pages#services", as: "services"
+end
